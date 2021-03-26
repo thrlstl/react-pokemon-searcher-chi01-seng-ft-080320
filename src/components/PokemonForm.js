6f8b343e444da9a0056ecedf795a52b861e97d54
thrlstl <matthewsteeleonline@gmail.com>
2021-03-26 15:22:14 -0500

    commit tree 4018f72df4746e4012524817a34ecda4f6d3b179

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,23 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form } from 'semantic-ui-react'
 
-class PokemonForm extends React.Component {
-  render() {
+function PokemonForm(props) {
+
+    const [data, setData] = useState({
+      name: '',
+      hp: 0,
+      sprites: {
+        front: '',
+        back: ''
+      }
+    })
+
+    const handleSubmit = () => {
+      props.addPokemon(data)
+    }
+
+    const handleChange = (value, field) => {
+        field === 'front' || field === 'back' ?
+        setData(prevData => ({
+          ...prevData,
+          sprites: { ...prevData.sprites, [field]: value}
+        })) :
+        setData({
+          ...data,
+          [field]: value
+        })
+    };
+    
     return (
       <div>
         <h3>Add a Pokemon!</h3>
-        <Form onSubmit={() => {console.log("submitting form...")}}>
+        <Form onSubmit={handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input onChange={(e) => handleChange(e.target.value, e.target.name)} fluid label="Name" placeholder="Name" name="name" />
+            <Form.Input onChange={(e) => handleChange(e.target.value, e.target.name)} fluid label="hp" placeholder="hp" name="hp" />
+            <Form.Input onChange={(e) => handleChange(e.target.value, e.target.name)} fluid label="Front Image URL" placeholder="url" name="front" />
+            <Form.Input onChange={(e) => handleChange(e.target.value, e.target.name)} fluid label="Back Image URL" placeholder="url" name="back" />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
       </div>
     )
   }
-}
 
 export default PokemonForm
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const Search = props => {
+function Search(props) {
+
+  const handleChange = e => {
+    props.setQuery(e)
+  }
+
   return (
     <div className="ui search">
       <div className="ui icon input">
-        <input className="prompt"/>
+        <input onChange={(e) => handleChange(e.target.value)} className="prompt"/>
         <i className="search icon" />
       </div>
     </div>
   )
 }
 
-export default Search
+export default Search;
